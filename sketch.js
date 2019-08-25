83d59d784e01382a71d5610af158f5f377e62866
HarshaSingh-pro <52882983+HarshaSingh-pro@users.noreply.github.com>
2019-08-25 12:51:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,46 @@
+//an array of same data types 
+
+var  arr1=[1,2,3,4,5];
+console.log(arr1);
+
+//different data types
+
+ var arr2=["Aryan",12345,true];
+ console.log(arr2[2]);
+
+//push
+arr2.push("harsha");
+console.log(arr2);
+
+//pop
+arr2.pop("harsha");
+console.log(arr2);
+
+
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
+
+
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
-    
-    ground = new Ground(600,height,1200,20)
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -28,16 +57,16 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
-    background(0);
+    background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
@@ -54,4 +83,22 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        slingshot.attach(bird.body);
+    }
 }
